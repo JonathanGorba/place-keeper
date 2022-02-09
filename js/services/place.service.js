d3c005ab7765cf9f1aed09b97cea2e067b4b23bc
Jonathan <yonatan5789@gmail.com>
2022-02-09 20:27:17 +0200

    yes

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -6,7 +6,7 @@ function initMap() {
     const uluru = { lat: 29.557383526537905, lng: 34.94998783298118 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 16,
       center: uluru,
     });
     // The marker, positioned at Uluru
